bd4c7ae6746213cee32462ef95f357dc770b2d1c
HovhannesMirzakhanyan <h.r.mirzakhanyan@gmail.com>
2021-11-06 18:37:58 +0400

    Add project files.

---
+++ UniversityManagment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityManagment.Models;
+using UniversityManagment.Services;
+
+
+namespace UniversityManagment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Sahak", 25);
+            Student student1 = new Student("Sahak", 25);
+            Student student2 = new Student("Sahak", 25);
+            Student student3 = new Student("Sahak", 25);
+            Student student4 = new Student("Sahak", 25);
+            Teacher teacher = new Teacher("Nune", 55);
+            StudentService service = new StudentService();
+            UniversityService uniService = new UniversityService(student,teacher);
+            uniService.AddTeacher(student, teacher);
+            service.Add(student);
+            service.Add(student1);
+            service.Add(student2);
+            service.Add(student3);
+            service.Add(student4);
+
+
+        }
+    }
+}
---
+++ UniversityManagment/Services/UniversityService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityManagment.Models;
+
+namespace UniversityManagment.Services
+{
+    public class UniversityService
+    {
+        private Student _student;
+        private Teacher _teacher;
+        private StudentService _studentService;
+        private TeacherService _teacherService;
+        public UniversityService(Student student, Teacher teacher)
+        {
+            _student = student;
+            _teacher = teacher;
+        }
+        public UniversityService(StudentService studentService, TeacherService teacherService)
+        {
+            _teacherService = teacherService;
+            _studentService = studentService;
+        }
+
+        public void AddTeacher(Student student, Teacher teacher)
+        {
+            _student._teacher = _teacher;
+        }
+
+    }
+}
